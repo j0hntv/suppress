19ae733671a2ce2537cb9701fca9ceff39840cc1
J0hn <j0hntvdesign@gmail.com>
2022-01-25 08:56:22 +0300

    Fix async support

--- suppress.py
+++ suppress.py
@@ -4,7 +4,7 @@ import contextlib
 from functools import wraps
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 def suppress(*exceptions):
@@ -17,8 +17,8 @@ def suppress(*exceptions):
     return wrap
 
 
-async def async_suppress(*exceptions):
-    async def wrap(func):
+def async_suppress(*exceptions):
+    def wrap(func):
         @wraps(func)
         async def inner(*args, **kwargs):
             with contextlib.suppress(exceptions):
